b995b17bd88ad123bd02a421e5273ec4b213dabb
Mauro Solcia <mauro.solcia@gmail.com>
2014-09-03 01:56:01 +0200

    Organized styles Added dark-side theme Updated dependencies with paper-elements Minimal app strucutre

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var element = document.querySelector(".content");
+/*var element = document.querySelector(".content");
 for (var i = 0; i < 200; i++) {
   element.innerText += "Hello, world!\n";
 }
+*/
+window.addEventListener('polymer-ready', function(e) {
+
+});
