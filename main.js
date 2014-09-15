3e7cbbcc7f2cf1b0c3b9842109f2e3a85c6784a3
Mauro Solcia <mauro.solcia@gmail.com>
2014-09-15 23:27:27 +0200

    Removed unused files Basic structure

--- main.js
+++
@@ -1,8 +0,0 @@
-/*var element = document.querySelector(".content");
-for (var i = 0; i < 200; i++) {
-  element.innerText += "Hello, world!\n";
-}
-*/
-window.addEventListener('polymer-ready', function(e) {
-
-});
